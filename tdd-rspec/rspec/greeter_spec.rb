97fa33958cbecf01d5750e4a01d6548a955e2cd7
Tayanne Bencardino <tayanne@Tayannes-MacBook-Air.local>
2022-06-10 14:51:45 +0100

    first commit

---
+++ tdd-rspec/rspec/greeter_spec.rb
@@ -0,0 +1,13 @@
+require 'greeter'
+
+describe 'Greeter' do
+  it 'greets Rico' do
+    expect(greet('Rico')).to eq 'Hello, Rico, how are you today?'
+  end
+end
+
+describe 'Greeter' do
+    it 'greets Chloe' do
+      expect(greet('Chloe')).to eq 'Hello, Chloe, how are you today?'
+    end
+  end
